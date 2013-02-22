6e899a81eddd5fc3f9bb0cef3b2ece26333d863f
Matthew Jewell <matt@maido.com>
2013-02-22 19:30:50 +0000

    moar committing

--- application/controllers/zombie.php
+++ application/controllers/zombie.php
@@ -9,15 +9,19 @@
 class Zombie extends CI_Controller {
 
     public function index() {
-        $this->load->model('Zombiemodel', '', TRUE);
-        echo json_encode($this->Zombiemodel->get_all_zombies());
-    }
-
-    public function nearest() {
         $lat  = @$_GET['lat'];
         $long = @$_GET['long'];
         $this->load->model('Zombiemodel', '', TRUE);
-        echo json_encode($this->Zombiemodel->get_nearest($lat, $long));
+        echo json_encode($this->Zombiemodel->get_all_zombies($lat, $long));
+    }
+
+    public function create() {
+        $zombie = array(
+            'lat' => @$_POST['lat'],
+            'lng' => @$_POST['lng']
+        );
+        $this->load->model('Zombiemodel', '', TRUE);
+        return json_encode($this->Zombiemodel->insert($zombie));
     }
 
 }
--- application/models/zombiemodel.php
+++ application/models/zombiemodel.php
@@ -12,14 +12,18 @@ class Zombiemodel extends CI_Model {
     public $latitude;
     public $reported_at;
 
-    public function get_all_zombies() {
-        $q = $this->db->query('SELECT * FROM zombies');
-        return $q->result_array();
-    }
+    public function get_all_zombies($lat = null, $long = null) {
+        if ($lat == null || $long == null) {
+            $q = $this->db->query('SELECT id, latitude as lat, longitude as lng,
+                                   UNIX_TIMESTAMP(reported_at) as reported_at
+                                   FROM zombies ORDER BY reported_at DESC');
+            return $q->result_array();
+        }
 
-    public function get_nearest($lat, $long) {
         $q = $this->db->query('
-           SELECT latitude, longitude, SQRT(
+           SELECT id, latitude as lat, longitude as `lng`,
+            UNIX_TIMESTAMP(reported_at) as reported_at
+            , SQRT(
                 POW(69.1 * (latitude - '.$lat.'), 2) +
                 POW(69.1 * ('.$long.' - longitude) * COS(latitude / 57.3), 2)) AS distance
             FROM zombies ORDER BY distance;
@@ -27,5 +31,22 @@ class Zombiemodel extends CI_Model {
         return $q->result_array();
     }
 
+    public function get_nearest_zombie ($lat, $long) {
+        $q = $this->db->query('
+           SELECT *, SQRT(
+                POW(69.1 * (latitude - '.$lat.'), 2) +
+                POW(69.1 * ('.$long.' - longitude) * COS(latitude / 57.3), 2)) AS distance
+            FROM zombies ORDER BY distance;
+            ');
+        return $q->row_array();
+    }
 
+    public function insert($zombie) {
+        $lat = $zombie['lat'];
+        $long = $zombie['lng'];
+        return $this->db->query('
+        INSERT INTO `zombies` (`latitude`, `longitude`)
+        VALUES ('.$lat.', '.$long.');
+        ');
+    }
 }
